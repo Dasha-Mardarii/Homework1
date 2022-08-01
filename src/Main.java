925d2a0af8a42ba675a80e145accdd3e8b71235c
Dasha-Mardarii <mardarii01@gmail.com>
2022-08-01 12:44:39 +0300

    homework part 1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        int x = 6;
-        int y = 10;
-        System.out.println(x + y);
+        int x = 23;
+        int y = 6;
+        System.out.println("x + y =" + (x + y));
+        System.out.println("x - y =" + (x - y));
+        System.out.println("x * y =" + (x * y));
+        System.out.println("x / y =" + (x / y));
+        System.out.println("x % y =" + (x % y));
+        System.out.println("x > y =" + (x > y));
+        int a = calculator(3, 5);
     }
 
+    static int calculator(int a, int b) {
+        return a * b - 3;
+
+    }
 }
+
+
